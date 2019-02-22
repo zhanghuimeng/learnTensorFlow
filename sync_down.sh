8bcebe9b40f4052fdb992ad03b51d2789bce8361
zhanghuimeng <zhanghuimeng1997@gmail.com>
2019-02-22 22:16:02 +0800

    修正了上传和下载方式

--- sync_down.sh
+++ sync_down.sh
@@ -1,1 +1,2 @@
-rsync -avz zhm@166.111.5.234:/data/disk4/private/zhm/201902learnTF/learnTensorFlow/ ~/Documents/learnTensorFlow
+rsync -avz --exclude '.idea' --exclude '.git' \
+    zhm@166.111.5.234:/data/disk4/private/zhm/201902learnTF/learnTensorFlow/ ~/Documents/learnTensorFlow
--- sync_up.sh
+++ sync_up.sh
@@ -1,1 +1,2 @@
-rsync -avz ~/Documents/learnTensorFlow/ zhm@166.111.5.234:/data/disk4/private/zhm/201902learnTF/learnTensorFlow
+rsync -avz --exclude '.idea' --exclude '.git' \
+    ~/Documents/learnTensorFlow/ zhm@166.111.5.234:/data/disk4/private/zhm/201902learnTF/learnTensorFlow
