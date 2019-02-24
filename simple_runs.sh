bd916d899f8d14ad19208fa28328d708b2a07a09
zhanghuimeng <zhanghuimeng1997@gmail.com>
2019-02-24 19:35:32 +0800

    增加了更多简单运行的内容

--- simple_runs.sh
+++ simple_runs.sh
@@ -2,13 +2,20 @@
 
 DATA_DIR=~/Documents/MT/projects/201810ineval_deen/data/processed
 
-while getopts "bs" arg
+while getopts "bqs" arg
 do
     case $arg in
         b)
             python seq2seq/build_vocab.py \
                 --corpus "$DATA_DIR/corpus.tc.32k.en.shuf" \
                 --name "data/test.en.vocab"
+            ;;
+        q)
+            python qe/qe.py \
+                --train data/qe-2017/train.src data/qe-2017/train.mt data/qe-2017/train.hter \
+                --vocab data/qe-2017/src.vocab data/qe-2017/tgt.vocab \
+                --dev data/qe-2017/dev.src data/qe-2017/dev.mt data/qe-2017/dev.hter \
+                --test data/qe-2017/test.src data/qe-2017/test.mt data/qe-2017/test.hter
             ;;
         s)
             python seq2seq/seq2seq.py \
